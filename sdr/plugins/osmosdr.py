27bd230b2af0439c60323e1df4c29805bf6797aa
Kevin Reid <kpreid@switchb.org>
2013-08-31 18:59:12 -0700

    Expose additional OsmoSDR settings.
    DC cancel, IQ balance, antenna, filter bandwidth.

--- sdr/plugins/osmosdr.py
+++ sdr/plugins/osmosdr.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import
 
 from sdr.source import Source
-from sdr.values import Cell, Range
+from sdr.values import Cell, Range, Enum
 
 import osmosdr
 
@@ -24,17 +24,17 @@ class OsmoSDRSource(Source):
 		
 		self.freq = freq = 98e6
 		self.correction_ppm = 0
+		self.dc_state = 0
+		self.iq_state = 0
 		
 		self.osmosdr_source_block = source = osmosdr.source("nchan=1 " + osmo_device)
 		# Note: Docs for these setters at gr-osmosdr/lib/source_iface.h
 		source.set_sample_rate(sample_rate)
 		source.set_center_freq(freq, ch)
 		# freq_corr: We implement correction internally because setting this at runtime breaks things
-		source.set_iq_balance_mode(0, ch)  # TODO
-		# gain_mode and gain: handled by accessors
-		source.set_antenna("", ch)  # n/a to RTLSDR
-		source.set_bandwidth(0, ch)  # TODO is this relevant
-		# Note: There is a DC cancel facility but it is not implemented for RTLSDR
+		source.set_dc_offset_mode(self.dc_state, ch)  # no getter, set to known state
+		source.set_iq_balance_mode(self.iq_state, ch)  # no getter, set to known state
+		# Note: DC cancel facility is not implemented for RTLSDR
 	
 		self.connect(self.osmosdr_source_block, self)
 	
@@ -44,13 +44,21 @@ class OsmoSDRSource(Source):
 	def state_def(self, callback):
 		super(OsmoSDRSource, self).state_def(callback)
 		# TODO: apply correction_ppm to freq range
-		# TODO: understand range gaps and artificially insert 0Hz range step
+		# TODO: understand range gaps and artificially insert 0Hz tuning point when applicable
 		callback(Cell(self, 'freq', writable=True, ctor=convert_osmosdr_range(
 			self.osmosdr_source_block.get_freq_range(ch), strict=False)))
 		callback(Cell(self, 'correction_ppm', writable=True, ctor=float))
-		callback(Cell(self, 'agc', writable=True, ctor=bool))
+		# TODO: Perhaps expose individual gain stages.
 		callback(Cell(self, 'gain', writable=True, ctor=convert_osmosdr_range(
 			self.osmosdr_source_block.get_gain_range(ch), strict=False)))
+		callback(Cell(self, 'agc', writable=True, ctor=bool))
+		callback(Cell(self, 'antenna', ctor=Enum(
+			{unicode(name): unicode(name) for name in self.osmosdr_source_block.get_antennas()})))
+		# Note: dc_cancel and iq_balance have 'manual' modes we are not exposing
+		callback(Cell(self, 'dc_cancel', writable=True, ctor=bool))
+		callback(Cell(self, 'iq_balance', writable=True, ctor=bool))
+		callback(Cell(self, 'bandwidth', writable=True, ctor=convert_osmosdr_range(
+			self.osmosdr_source_block.get_bandwidth_range(ch))))
 		
 	def get_sample_rate(self):
 		# TODO review why cast
@@ -68,6 +76,7 @@ class OsmoSDRSource(Source):
 
 		self.freq = freq
 		self._update_frequency()
+		# TODO: According to OsmoSDR's interface, the frequency tuned to (get_center_freq) may not be the one we requested. Handle that.
 
 	def get_tune_delay(slf):
 		return 0.25  # TODO: make configurable and/or account for as many factors as we can
@@ -95,19 +104,56 @@ class OsmoSDRSource(Source):
 		
 		self.tune_hook()
 
-	def get_agc(self):
-		return bool(self.osmosdr_source_block.get_gain_mode(ch))
-
-	def set_agc(self, value):
-		self.osmosdr_source_block.set_gain_mode(bool(value), ch)
-	
 	def get_gain(self):
 		return self.osmosdr_source_block.get_gain(ch)
 	
 	def set_gain(self, value):
 		self.osmosdr_source_block.set_gain(float(value), ch)
+	
+	def get_agc(self):
+		return bool(self.osmosdr_source_block.get_gain_mode(ch))
+	
+	def set_agc(self, value):
+		self.osmosdr_source_block.set_gain_mode(bool(value), ch)
+	
+	def get_antenna(self):
+		return unicode(self.osmosdr_source_block.get_antenna(ch))
+		# TODO review whether set_antenna is safe to expose
+	
+	def get_dc_cancel(self):
+		return bool(self.dc_state)
+	
+	def set_dc_cancel(self, value):
+		self.dc_state = bool(value)
+		if self.dc_state:
+			mode = 2  # automatic mode
+		else:
+			mode = 0
+		self.osmosdr_source_block.set_dc_offset_mode(mode, ch)
+	
+	def get_iq_balance(self):
+		return bool(self.iq_state)
+	
+	def set_iq_balance(self, value):
+		self.iq_state = bool(value)
+		if self.iq_state:
+			mode = 2  # automatic mode
+		else:
+			mode = 0
+		self.osmosdr_source_block.set_iq_balance_mode(mode, ch)
+	
+	def get_bandwidth(self):
+		return self.osmosdr_source_block.get_bandwidth(ch)
+	
+	def set_bandwidth(self, value):
+		self.osmosdr_source_block.set_bandwidth(float(value), ch)
 
 
-def convert_osmosdr_range(range, **kwargs):
-	# Note: range may have gaps and we don't yet represent that
-	return Range(range.start(), range.stop(), **kwargs)
+def convert_osmosdr_range(meta_range, **kwargs):
+	if len(meta_range.values()) == 0:
+		# If we don't check this condition, start() and stop() will raise
+		# TODO better stub value
+		return Range(-1, -1, **kwargs)
+	else:
+		# Note: meta_range may have gaps and we don't yet represent that
+		return Range(meta_range.start(), meta_range.stop(), **kwargs)
