ffdc8b7630e18764fd30be1cc980e35738d8dae4
Kevin Reid <kpreid@switchb.org>
2013-11-17 15:10:16 -0800

    Compatibility fix for fetch-js-deps.

--- fetch-js-deps.sh
+++ fetch-js-deps.sh
@@ -12,10 +12,11 @@ git submodule update --init
 
 ol_version="2.13.1"
 ol_file="OpenLayers-$ol_version.tar.gz"
+ol_unpack="OpenLayers-$ol_version"
 (cd shinysdr/deps/ &&
   wget -N "http://openlayers.org/download/$ol_file" &&
   tar zxf "$ol_file" &&
-  rm -r openlayers &&
+  rm -rf openlayers &&
   mkdir openlayers &&
-  mv "OpenLayers-$ol_version/"{img,theme,OpenLayers*.js} openlayers/ &&
-  rm -rf "OpenLayers-$ol_version/" "$ol_file")
+  mv "$ol_unpack/img" "$ol_unpack/theme" "$ol_unpack"/OpenLayers*.js openlayers/ &&
+  rm -rf "$ol_unpack" "$ol_file")
