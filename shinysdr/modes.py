8f4f5df5527f04c259fb63b6b67332196a818f6f
Kevin Reid <kpreid@switchb.org>
2014-04-12 21:35:36 -0700

    Quiet pylint some more.

--- shinysdr/modes.py
+++ shinysdr/modes.py
@@ -15,6 +15,9 @@
 # You should have received a copy of the GNU General Public License
 # along with ShinySDR.  If not, see <http://www.gnu.org/licenses/>.
 
+# pylint: disable=no-method-argument, no-init
+# (pylint is confused by interfaces)
+
 from __future__ import absolute_import, division
 
 from twisted.plugin import IPlugin, getPlugins
