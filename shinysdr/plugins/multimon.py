452c180e42501f101cea67be251bc4126efc2471
Kevin Reid <kpreid@switchb.org>
2014-07-06 16:44:17 -0700

    Separate multimon glue from FM demodulation.

--- shinysdr/plugins/multimon.py
+++ shinysdr/plugins/multimon.py
@@ -28,8 +28,8 @@ from zope.interface import implements
 from gnuradio import gr
 from gnuradio import blocks
 
-from shinysdr.modes import ModeDef, IDemodulator
 from shinysdr.blocks import make_sink_to_process_stdin, test_subprocess, make_resampler
+from shinysdr.modes import ModeDef, IDemodulator
 from shinysdr.plugins.basic_demod import NFMDemodulator
 from shinysdr.plugins.aprs import APRSInformation, parse_tnc2
 from shinysdr.signals import SignalType
@@ -42,18 +42,16 @@ int_scale = _maxint32 * audio_gain
 
 
 class MultimonNGDemodulator(gr.hier_block2, ExportedState):
-	implements(IDemodulator)
+	# This is not an IDemodulator; it takes float input, requires a fixed input rate and lacks other characteristics.
 	
-	def __init__(self, mode, input_rate=0, aprs_information=None, context=None):
-		assert input_rate > 0
+	def __init__(self, mode, aprs_information=None, context=None):
 		gr.hier_block2.__init__(
 			self, str(mode) + ' (Multimon-NG) demodulator',
-			gr.io_signature(1, 1, gr.sizeof_gr_complex * 1),
+			gr.io_signature(1, 1, gr.sizeof_float * 1),
 			# TODO: Add generic support for demodulators with no audio output
 			gr.io_signature(1, 1, gr.sizeof_float * 1),
 		)
 		self.mode = mode
-		self.input_rate = input_rate
 		
 		if aprs_information is not None:
 			self.__information = aprs_information
@@ -65,13 +63,6 @@ class MultimonNGDemodulator(gr.hier_block2, ExportedState):
 			message = parse_tnc2(line, time.time())
 			log.msg(u'   -> %s' % (message,))
 			self.__information.receive(message)
-		
-		# FM demod
-		self.fm_demod = NFMDemodulator(
-			mode='NFM',
-			input_rate=input_rate,
-			tau=None)  # no deemphasis
-		fm_audio_rate = self.fm_demod.get_output_type().get_sample_rate()
 		
 		# Subprocess
 		# using /usr/bin/env because twisted spawnProcess doesn't support path search
@@ -88,18 +79,72 @@ class MultimonNGDemodulator(gr.hier_block2, ExportedState):
 		sink = make_sink_to_process_stdin(process, itemsize=gr.sizeof_short)
 		
 		# Output
-		converter = blocks.float_to_short(vlen=1, scale=int_scale)
+		to_short = blocks.float_to_short(vlen=1, scale=int_scale)
 		self.connect(
 			self,
-			self.fm_demod,
-			make_resampler(fm_audio_rate, pipe_rate),
-			converter,
+			# blocks.complex_to_float(),
+			to_short,
 			sink)
 		# Audio copy output
 		unconverter = blocks.short_to_float(vlen=1, scale=int_scale)
-		self.connect(converter, unconverter)
+		self.connect(to_short, unconverter)
 		self.connect(unconverter, self)
 		
+	def can_set_mode(self, mode):
+		return False
+	
+	def get_input_type(self):
+		return SignalType(kind='MONO', sample_rate=pipe_rate)
+	
+	def get_output_type(self):
+		return SignalType(kind='MONO', sample_rate=pipe_rate)
+	
+	def get_half_bandwidth(self):
+		return pipe_rate / 2
+
+
+# TODO: Eliminate this class and replace it with adapters available to any demodulator
+class FMMultimonNGDemodulator(gr.hier_block2, ExportedState):
+	implements(IDemodulator)
+	
+	def __init__(self, mode, input_rate=0, aprs_information=None, context=None):
+		assert input_rate > 0
+		gr.hier_block2.__init__(
+			self, str(mode) + ' (FM + Multimon-NG) demodulator',
+			gr.io_signature(1, 1, gr.sizeof_gr_complex * 1),
+			gr.io_signature(1, 1, gr.sizeof_float * 1),
+		)
+		self.mode = mode
+		self.input_rate = input_rate
+		
+		# FM demod
+		# TODO: Retry telling the NFMDemodulator to have its output rate be pipe_rate instead of using a resampler. Something went wrong when trying that before.
+		self.fm_demod = NFMDemodulator(
+			mode='NFM',
+			input_rate=input_rate,
+			tau=None)  # no deemphasis
+		assert self.fm_demod.get_output_type().get_kind() == 'MONO'
+		fm_audio_rate = self.fm_demod.get_output_type().get_sample_rate()
+		
+		# Subprocess
+		self.mm_demod = MultimonNGDemodulator(
+			mode='APRS',
+			aprs_information=aprs_information)
+		mm_audio_rate = self.mm_demod.get_input_type().get_sample_rate()
+		
+		# Output
+		self.connect(
+			self,
+			self.fm_demod,
+			make_resampler(fm_audio_rate, mm_audio_rate),
+			self.mm_demod,
+			self)
+	
+	def state_def(self, callback):
+		super(FMMultimonNGDemodulator, self).state_def(callback)
+		# TODO make this possible to be decorator style
+		callback(BlockCell(self, 'mm_demod'))
+	
 	def can_set_mode(self, mode):
 		return False
 	
@@ -107,7 +152,7 @@ class MultimonNGDemodulator(gr.hier_block2, ExportedState):
 		return self.fm_demod.get_half_bandwidth()
 	
 	def get_output_type(self):
-		return SignalType(kind='MONO', sample_rate=pipe_rate)
+		return self.mm_demod.get_output_type()
 	
 	@exported_value()
 	def get_band_filter_shape(self):
@@ -149,9 +194,12 @@ class MultimonNGProcessProtocol(ProcessProtocol):
 
 
 # TODO: Arrange for a way for the user to see why it is unavailable.
+_multimon_available = test_subprocess('multimon-ng -h; exit 0', 'available demodulators:', shell=True)
+
+
 pluginDef_APRS = ModeDef(
-	mode='APRS',
+	mode='APRS',  # TODO: Rename mode to be more accurate
 	label='APRS',
-	demod_class=MultimonNGDemodulator,
+	demod_class=FMMultimonNGDemodulator,
 	shared_objects={'aprs_information': APRSInformation},
-	available=test_subprocess('multimon-ng -h; exit 0', 'available demodulators:', shell=True))
+	available=_multimon_available)
