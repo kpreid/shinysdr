e03b4a5bec2122b2817ad73e84973aa9db76fec8
Kevin Reid <kpreid@switchb.org>
2014-09-01 11:08:55 -0700

    Add some test cases for shinysdr.math.

---
+++ shinysdr/test/test_math.py
@@ -0,0 +1,48 @@
+# Copyright 2014 Kevin Reid <kpreid@switchb.org>
+# 
+# This file is part of ShinySDR.
+# 
+# ShinySDR is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# ShinySDR is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with ShinySDR.  If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import absolute_import, division
+
+from twisted.trial import unittest
+
+import shinysdr.math as smath
+
+
+class TestFactorize(unittest.TestCase):
+	longMessages = True
+	
+	def test_error(self):
+		self.assertRaises(ValueError, lambda: smath.factorize(0))
+	def test_cases(self):
+		self.assertEqual(smath.factorize(1), [])
+		self.assertEqual(smath.factorize(2), [2])
+		self.assertEqual(smath.factorize(3), [3])
+		self.assertEqual(smath.factorize(4), [2, 2])
+		self.assertEqual(smath.factorize(5), [5])
+		self.assertEqual(smath.factorize(6), [2, 3])
+		self.assertEqual(smath.factorize(7), [7])
+		self.assertEqual(smath.factorize(8), [2, 2, 2])
+		self.assertEqual(smath.factorize(9), [3, 3])
+		self.assertEqual(smath.factorize(48000), [2] * 7 + [3] + [5] * 3)
+
+class TestSmallFactorAtLeast(unittest.TestCase):
+	longMessages = True
+	
+	def test_cases(self):
+		self.assertEqual(smath.small_factor_at_least(100, 9), 25)  # known flaw
+		self.assertEqual(smath.small_factor_at_least(100, 10), 10)
+		self.assertEqual(smath.small_factor_at_least(100, 11), 25)
