089d35a27351e121cbb2865cde99d3de00176136
Kevin Reid <kpreid@switchb.org>
2016-08-15 15:40:31 -0700

    "Controller" plugin: sends user-defined commands to some endpoint.
    Should be documented in the manual, but isn't, mainly because I'm not
    quite happy with the overall idea and the configuration syntax.

---
+++ shinysdr/plugins/controller.py
@@ -0,0 +1,167 @@
+# Copyright 2016 Kevin Reid <kpreid@switchb.org>
+#
+# This file is part of ShinySDR.
+# 
+# ShinySDR is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# ShinySDR is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with ShinySDR.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+Plugin for user-defined arbitrary commands.
+
+It connects to something (such as a serial port) and sends arbitrary strings defined in configuration in response to user input.
+"""
+# TODO: Write user documentation for this device. Maybe think about making it easier to configure first.
+
+from __future__ import absolute_import, division
+
+import functools
+
+from twisted.internet.protocol import Factory, Protocol
+from twisted.protocols.basic import LineReceiver
+from zope.interface import implements, Interface
+
+from shinysdr.devices import Device, IComponent
+from shinysdr.types import to_value_type
+from shinysdr.values import Command as CommandCell, ExportedState, LooseCell
+
+
+__all__ = []  # appended later
+
+
+def Controller(reactor, key='controller', endpoint=None, elements=None, encoding='US-ASCII'):
+    """Create a controller device.
+  
+    key: Component ID. TODO point at device merging documentation once it exists
+    endpoint: Endpoint to connect to (such as a shinysdr.twisted_ext.SerialPortEndpoint).
+    elements: List of elements (objects which define commands to send).
+    encoding: Character encoding to use when Unicode text is given.
+    """
+    return Device(components={unicode(key): _ControllerProxy(
+        reactor=reactor,
+        endpoint=endpoint,
+        elements=elements or [],
+        encoding=encoding)})
+
+
+__all__.append('Controller')
+
+
+class IElement(Interface):
+    def _install_cells(callback, send, encoding):
+        pass
+
+
+class Command(object):
+    """Defines a command to send a single string."""
+    implements(IElement)
+    
+    def __init__(self, label, text):
+        """label: Name the user sees.
+        text: What is sent when the command is triggered.
+        """
+        if not (isinstance(text, str) or isinstance(text, unicode)):
+            raise TypeError('Command text must be string, not %s: %r' % (type(text), text))
+        self.__label = label
+        self.__text = text
+    
+    def _install_cells(self, callback, send, encoding):
+        if isinstance(self.__text, unicode):
+            text = self.__text.encode(encoding)
+        else:
+            text = self.__text
+        # TODO: Use cell metadata (once that idea exists) to specify the label, so it isn't required to be unique.
+        callback(CommandCell(self, self.__label, functools.partial(send, text)))
+
+
+__all__.append('Command')
+
+
+class Selector(object):
+    """Defines a cell whose value is the text to send.
+    
+    Typically the cell's type would be an Enum.
+    """
+    implements(IElement)
+    
+    def __init__(self, name, type):
+        # pylint: disable=redefined-builtin
+        self.__name = name
+        self.__type = to_value_type(type)
+    
+    def _install_cells(self, callback, send, encoding):
+        callback(LooseCell(
+            key=self.__name,
+            type=self.__type,
+            value=u'',
+            writable=True,
+            persists=True,
+            post_hook=lambda value: send(unicode(value).encode(encoding))))
+
+
+__all__.append('Selector')
+
+
+class _ControllerProxy(ExportedState):
+    implements(IComponent)
+    
+    def __init__(self, reactor, endpoint, elements, encoding):
+        self.__reactor = reactor
+        self.__elements = elements
+        self.__encoding = encoding
+        
+        # ClientService (which does reconnecting) is only since 16.1 and we currently want to support 13.2 due to MacPorts and Debian versions
+        # cs = ClientService(endpoint, Factory.forProtocol(_ControllerProtocol))
+        # cs.startService()
+        factory = Factory.forProtocol(_ControllerProtocol)
+        self.__protocol = None
+        endpoint.connect(factory).addCallback(self.__got_protocol)
+    
+    def state_def(self, callback):
+        for element in self.__elements:
+            IElement(element)._install_cells(callback, self.__send, self.__encoding)
+    
+    def close(self):
+        # TODO: This is used for testing and is not actually called by Device.close. Device.close needs to be extended to support notifying components of close.
+        if self.__protocol:
+            self.__protocol.transport.loseConnection()
+    
+    def __got_protocol(self, protocol):
+        self.__protocol = protocol
+    
+    def __send(self, cmd):
+        self.__protocol.send(cmd)
+
+
+class _ControllerProtocol(Protocol):
+    def __init__(self):
+        self.__line_receiver = LineReceiver()
+        self.__line_receiver.delimiter = ';'
+        self.__line_receiver.lineReceived = self.__lineReceived
+    
+    def connectionMade(self):
+        """overrides Protocol"""
+        # TODO: Report success
+    
+    def connectionLost(self, reason=None):
+        """overrides Protocol"""
+        # TODO: Report loss to user
+    
+    def dataReceived(self, data):
+        """overrides Protocol"""
+        self.__line_receiver.dataReceived(data)
+    
+    def __lineReceived(self, line):
+        print line
+    
+    def send(self, cmd):
+        self.transport.write(cmd)
---
+++ shinysdr/test/test_controller.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Kevin Reid <kpreid@switchb.org>
+# 
+# This file is part of ShinySDR.
+# 
+# ShinySDR is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# ShinySDR is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with ShinySDR.  If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import absolute_import, division
+
+from twisted.test.proto_helpers import StringTransport
+from twisted.trial import unittest
+from twisted.internet import defer
+from twisted.internet.interfaces import IStreamClientEndpoint
+from twisted.internet import reactor as the_reactor
+from zope.interface import implements
+
+from shinysdr.plugins.controller import Controller, Command, Selector
+from shinysdr.test.testutil import state_smoke_test
+from shinysdr.types import Enum
+
+
+class TestController(unittest.TestCase):
+    """
+    Also contains generic proxy tests.
+    """
+    timeout = 5
+    
+    def setUp(self):
+        self.endpoint = _StringEndpoint()
+        self.device = Controller(
+            reactor=the_reactor,
+            endpoint=self.endpoint,
+            elements=[
+                Command('cmd_name', 'cmd_text'),
+                Command('unicode_cmd', u'façade'),
+                Selector('enum_name', Enum({u'enum_text1': u'enum_label1', u'enum_text2': u'enum_label2'}, strict=False))
+            ],
+            encoding='UTF-8')
+        self.proxy = self.device.get_components_dict()['controller']
+    
+    @defer.inlineCallbacks
+    def tearDown(self):
+        yield self.device.close()
+        yield self.proxy.close()  # TODO kludge because device close doesn't actually work for our purposes
+    
+    def test_state_smoke(self):
+        state_smoke_test(self.device)
+    
+    def test_send_command(self):
+        self.proxy.state()['cmd_name'].set(True)  # TODO command-cell kludge
+        self.assertEqual('cmd_text', self.endpoint.t.value())
+    
+    def test_send_enum(self):
+        self.proxy.state()['enum_name'].set('enum_text1')
+        self.endpoint.t.clear()
+        self.proxy.state()['enum_name'].set('enum_text2')
+        self.assertEqual('enum_text2', self.endpoint.t.value())
+    
+    def test_encode_command(self):
+        self.proxy.state()['unicode_cmd'].set(True)  # TODO command-cell kludge
+        self.assertEqual(u'façade'.encode('UTF-8'), self.endpoint.t.value())
+    
+    def test_encode_enum(self):
+        self.proxy.state()['enum_name'].set(u'façade')
+        self.assertEqual(u'façade'.encode('UTF-8'), self.endpoint.t.value())
+
+class _StringEndpoint(object):
+    implements(IStreamClientEndpoint)
+    
+    def __init__(self):
+        self.t = StringTransport()
+    
+    def connect(self, protocol_factory):
+        protocol = protocol_factory.buildProtocol(None)
+        protocol.makeConnection(self.t)
+        return defer.succeed(protocol)
