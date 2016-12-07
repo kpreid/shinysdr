139f39e5427c2870eead858ecf53862251234dbc
Kevin Reid <kpreid@switchb.org>
2016-12-06 19:41:49 -0800

    Remove cruft from audio-fft code.

--- shinysdr/i/webstatic/tools/audio-fft-main.js
+++ shinysdr/i/webstatic/tools/audio-fft-main.js
@@ -15,46 +15,36 @@
 // You should have received a copy of the GNU General Public License
 // along with ShinySDR.  If not, see <http://www.gnu.org/licenses/>.
 
-// TODO: remove network module depenency
 require.config({
   baseUrl: '/client/'
 });
-define(['audio', 'types', 'values', 'events', 'widget', 'widgets', 'network', 'database', 'coordination'], function (audio, types, values, events, widget, widgets, network, database, coordination) {
+define(['audio', 'coordination', 'events', 'types', 'values', 'widget', 'widgets'],
+       ( audio,   coordination,   events,   types,   values,   widget,   widgets ) => {
   'use strict';
   
-  var AudioAnalyserAdapter = audio.AudioAnalyserAdapter;
-  var ClientStateObject = coordination.ClientStateObject;
-  var ConstantCell = values.ConstantCell;
-  var makeBlock = values.makeBlock;
+  const scheduler = new events.Scheduler();
+  const audioContext = new AudioContext();
   
-  var scheduler = new events.Scheduler();
-  
-  var ctx = new AudioContext();
-  var sampleRate = ctx.sampleRate;
-  
-  var adapter = new AudioAnalyserAdapter(scheduler, ctx);
+  const adapter = new audio.AudioAnalyserAdapter(scheduler, audioContext);
   adapter.paused.set(false);
   
   navigator.mediaDevices.getUserMedia({audio: true}).then(function getUserMediaSuccess(stream) {
-    var source = ctx.createMediaStreamSource(stream);
+    const source = audioContext.createMediaStreamSource(stream);
     adapter.connectFrom(source);
   }, function getUserMediaFailure(e) {
-    var d = document.createElement('dialog');
+    const d = document.createElement('dialog');
     // e is a DOMException
     d.textContent = 'Failed to get audio input: ' + e.name;
     document.body.appendChild(d);
     d.show();
   });
   
-  var root = new ConstantCell(types.block, makeBlock({
-    monitor: new ConstantCell(types.block, adapter)
-  }));
+  const root = new values.ConstantCell(types.block, adapter);
   
-  var context = new widget.Context({
+  const context = new widget.Context({
     widgets: widgets,
-    clientState: new ClientStateObject(sessionStorage, null),  // TODO: using sessionStorage as an approximation for "no storage".
-    spectrumView: null,
-    freqDB: new database.Union(),
+    // Using sessionStorage because we want default settings and because our storage usage doesn't yet distinguish between different pages.
+    clientState: new coordination.ClientStateObject(sessionStorage, null),
     scheduler: scheduler
   });
   
