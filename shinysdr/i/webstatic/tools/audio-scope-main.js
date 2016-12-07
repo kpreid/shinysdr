184f6e9b9e04bc148ad59f784bf1c34ce66fa926
Kevin Reid <kpreid@switchb.org>
2016-12-06 19:29:12 -0800

    Create audio-scope page, parallel to audio-fft.

---
+++ shinysdr/i/webstatic/tools/audio-scope-main.js
@@ -0,0 +1,48 @@
+// Copyright 2014, 2015, 2016 Kevin Reid <kpreid@switchb.org>
+// 
+// This file is part of ShinySDR.
+// 
+// ShinySDR is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// ShinySDR is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with ShinySDR.  If not, see <http://www.gnu.org/licenses/>.
+
+require.config({
+  baseUrl: '/client/'
+});
+define(['audio', 'coordination', 'events', 'types', 'values', 'widget', 'widgets'],
+       ( audio,   coordination,   events,   types,   values,   widget,   widgets ) => {
+  'use strict';
+  
+  const scheduler = new events.Scheduler();
+  const audioContext = new AudioContext();
+  
+  const adapter = new audio.AudioScopeAdapter(scheduler, audioContext);
+  
+  audio.getUserMediaForAudioTools(audioContext).then((source) => {
+    if (source === null) { 
+      // Error reporting already handled.
+      return;
+    }
+    adapter.connectFrom(source);
+  })
+  
+  const root = new values.ConstantCell(types.block, adapter);
+  
+  const context = new widget.Context({
+    widgets: widgets,
+    // Using sessionStorage because we want default settings and because our storage usage doesn't yet distinguish between different pages.
+    clientState: new coordination.ClientStateObject(sessionStorage, null),
+    scheduler: scheduler
+  });
+  
+  widget.createWidgets(root, context, document);
+});
