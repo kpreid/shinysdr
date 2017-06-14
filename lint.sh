100a7cc6a496964a588e65c02faf9fa0ca28fca3
Kevin Reid <kpreid@switchb.org>
2017-06-14 10:11:29 -0700

    Have lint.sh generate a summary status code.

--- lint.sh
+++ lint.sh
@@ -1,9 +1,23 @@
-#!/bin/sh
+#!/bin/bash
+
+set -e
+declare -a errors
+
+function linter {
+  echo "------ $0: Running $1"
+  "$@" || errors+=("$1")
+}
 
 # JS lint
-jshint shinysdr/i/{webstatic,webparts}
+linter jshint shinysdr/i/{webstatic,webparts}
 
 # Python lint
 # pylint is last because it is the slowest linter.
-flake8 --exclude=deps shinysdr/ *.py
-pylint --rcfile pylintrc shinysdr
+linter flake8 --exclude=deps shinysdr/ *.py
+linter pylint --rcfile pylintrc shinysdr
+
+# Print summary and return status code
+if [[ ${#errors[*]} -ne 0 ]]; then
+  echo "------ $0: Errors found by: ${errors[@]}"
+  exit 1
+fi
