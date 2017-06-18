5b43d7e7756c6070b05079f923224740b21274c6
Kevin Reid <kpreid@switchb.org>
2017-06-18 13:28:26 -0700

    Allow running lint for only one language.
    This is mainly to not need to wait for Pylint for JS changes.

--- lint.sh
+++ lint.sh
@@ -1,23 +1,35 @@
 #!/bin/bash
 
 set -e
-declare -a errors
+declare -a errors skips
+
+only_lang="${1:-all}"
 
 function linter {
-  echo "------ $0: Running $1"
-  "$@" || errors+=("$1")
+  local lang="$1"; shift
+  local program="$1"; shift
+  if [[ "x$only_lang" == "x$lang" || "x$only_lang" == "xall" ]]; then
+    echo "------ $0: Running $program ($lang)"
+    "$program" "$@" || errors+=("$program")
+  else
+    skips+=("$program")
+  fi
 }
 
 # JS lint
-linter jshint shinysdr/i/{webstatic,webparts}
+linter js jshint shinysdr/i/{webstatic,webparts}
 
 # Python lint
 # pylint is last because it is the slowest linter.
-linter flake8 --exclude=deps shinysdr/ *.py
-linter pylint --rcfile pylintrc shinysdr
+linter py flake8 --exclude=deps shinysdr/ *.py
+linter py pylint --rcfile pylintrc shinysdr
 
 # Print summary and return status code
 if [[ ${#errors[*]} -ne 0 ]]; then
   echo "------ $0: Errors found by: ${errors[@]}"
   exit 1
+fi
+if [[ ${#skips[*]} -ne 0 ]]; then
+  echo "------ $0: Skipped: ${skips[@]}"
+  exit 2
 fi
