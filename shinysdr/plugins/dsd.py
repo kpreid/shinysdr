303ede61c51e70b91b2da6b062a65dd832a814b1
Kevin Reid <kpreid@switchb.org>
2018-01-26 08:41:40 -0800

    DSD parameter setting, uvquality only for now.

--- shinysdr/plugins/dsd.py
+++ shinysdr/plugins/dsd.py
@@ -32,6 +32,7 @@ try:
 except ImportError:
     try:
         from dsd import block_ff as dsd_block_ff
+        import dsd
         _available_version = 1
     except ImportError:
         _available_version = None
@@ -40,24 +41,29 @@ from shinysdr.filters import make_resampler
 from shinysdr.interfaces import BandShape, ModeDef, IDemodulator
 from shinysdr.plugins.basic_demod import NFMDemodulator
 from shinysdr.signals import SignalType
-from shinysdr.types import EnumRow, ReferenceT
-from shinysdr.values import ExportedState, exported_value
+from shinysdr.types import EnumRow, RangeT, ReferenceT
+from shinysdr.values import ExportedState, exported_value, setter
 
 
 _debug_print = True  # TODO turn this off
 _demod_rate = 48000  # hardcoded in gr-dsd
+_uvquality_range = RangeT([(1, 4)], integer=True)
 
 
 @implementer(IDemodulator)
 class DSDDemodulator(gr.hier_block2, ExportedState):
-    def __init__(self, mode, input_rate=0, context=None):
+    def __init__(self, mode, input_rate=0, uvquality=3, context=None):
         assert input_rate > 0
         gr.hier_block2.__init__(
             self, type(self).__name__,
             gr.io_signature(1, 1, gr.sizeof_gr_complex),
             gr.io_signature(1, 1, gr.sizeof_float))
         
-        # TODO: Retry telling the NFMDemodulator to have its output rate be pipe_rate instead of using a resampler. Something went wrong when trying that before. Same thing is done in multimon.py
+        self.__context = context
+        self.__output_type = SignalType(kind='MONO', sample_rate=8000)
+        self.__uvquality = uvquality
+        
+        # TODO: Retry telling the NFMDemodulator to have its output rate be _demod_rate instead of using a resampler. Something went wrong when trying that before. Same thing is done in multimon.py
         self.__fm_demod = NFMDemodulator(
             mode='NFM',
             input_rate=input_rate,
@@ -65,29 +71,52 @@ class DSDDemodulator(gr.hier_block2, ExportedState):
             tau=None)  # no deemphasis
         assert self.__fm_demod.get_output_type().get_kind() == 'MONO'
         fm_audio_rate = self.__fm_demod.get_output_type().get_sample_rate()
-
-        self.__output_type = SignalType(kind='MONO', sample_rate=8000)
+        self.__resampler = make_resampler(fm_audio_rate, _demod_rate)
         
-        if _available_version == 1:
-            # backwards compatibility
-            decoder = dsd_block_ff()
-        else:
-            decoder = dsd_block_ff(
-                frame=dsd.dsd_FRAME_AUTO_DETECT,
-                mod=dsd.dsd_MOD_AUTO_SELECT,
-                uvquality=3,  # TODO figure out what this does
-                errorbars=_debug_print,
-                verbosity=2 if _debug_print else 0)
-        self.connect(
-            self,
-            self.__fm_demod,
-            make_resampler(fm_audio_rate, _demod_rate),
-            decoder,
-            self)
+        self.__do_connect()
+    
+    def __do_connect(self):
+        self.__context.lock()
+        try:
+            self.disconnect_all()
+            if _available_version == 1:
+                # backwards compatibility
+                decoder = dsd_block_ff()
+            else:
+                decoder = dsd_block_ff(
+                    # TODO: Add controls to choose frame and mod options at runtime — need to be able to get the enum info from gr-dsd, which may not even be currently available.
+                    frame=dsd.dsd_FRAME_AUTO_DETECT,
+                    mod=dsd.dsd_MOD_AUTO_SELECT,
+                    uvquality=self.__uvquality,
+                    errorbars=_debug_print,
+                    verbosity=2 if _debug_print else 0)
+            self.connect(
+                self,
+                self.__fm_demod,
+                self.__resampler,
+                decoder,
+                self)
+        finally:
+            self.__context.unlock()
     
     @exported_value(type=ReferenceT(), changes='never')
     def get_fm_demod(self):
         return self.__fm_demod
+    
+    if _available_version >= 2:
+        @exported_value(type=_uvquality_range,
+            changes='this_setter',
+            label='Unvoiced speech quality',
+            parameter='uvquality')
+        def get_uvquality(self):
+            return self.__uvquality
+    
+        @setter
+        def set_uvquality(self, value):
+            value = _uvquality_range(value)
+            if self.__uvquality != value:
+                self.__uvquality = value
+                self.__do_connect()
     
     def get_output_type(self):
         return self.__output_type
